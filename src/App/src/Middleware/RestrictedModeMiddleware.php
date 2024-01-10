c8ac243e81ddc7439ec4229fd360598c21c235f4
Ersin Güvenç <eguvenc@gmail.com>
2024-01-10 21:32:46 +0300

    updates

--- src/App/src/Middleware/RestrictedModeMiddleware.php
+++ src/App/src/Middleware/RestrictedModeMiddleware.php
@@ -23,27 +23,27 @@ class RestrictedModeMiddleware implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface
     {        
-        // $currentPath =  $request->getUri()->getPath();
-        // $allowedRoutes = [
-        //     "/api/auth/token",
-        //     "/api/auth/session",
-        //     "/api/auth/refresh",
-        //     "/api/auth/logout",
-        // ];
-        // if (! in_array($currentPath, $allowedRoutes) 
-        //     && in_array($request->getMethod(), ["POST", "PUT", "DELETE"])
-        // ) {
-        //     return new JsonResponse(
-        //         [
-        //             'data' => [
-        //                 'error' => $this->translator->translate(
-        //                     "Demo mode can only handle read operations. To perform writes, install the demo application in your environment and remove the RestrictedModeMiddleware class from config/pipeline.php"
-        //                 )
-        //             ]
-        //         ],
-        //         400
-        //     );
-        // }
+        $currentPath =  $request->getUri()->getPath();
+        $allowedRoutes = [
+            "/api/auth/token",
+            "/api/auth/session",
+            "/api/auth/refresh",
+            "/api/auth/logout",
+        ];
+        if (! in_array($currentPath, $allowedRoutes) 
+            && in_array($request->getMethod(), ["POST", "PUT", "DELETE"])
+        ) {
+            return new JsonResponse(
+                [
+                    'data' => [
+                        'error' => $this->translator->translate(
+                            "Demo mode can only handle read operations. To perform writes, install the demo application in your environment and remove the RestrictedModeMiddleware class from config/pipeline.php"
+                        )
+                    ]
+                ],
+                400
+            );
+        }
         return $handler->handle($request);
     }
 }
